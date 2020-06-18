dafac775c5ee4005afb55f2134386725956c35e5
mudit67 <bhardwajmudit006@gmail.com>
2020-06-18 23:02:51 +0530

    Moved all the specifications of the recipes(Dishes) to dishes.js folder. Lifted the State up to app.js. set an onclick attribute to display description of the click dish to be shown at the bottom.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponents'
 import './App.css';
+import {DISHES} from './shared/dishes';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/" >
-            Ristorante Con Fusion
-          </NavbarBrand>
-        </div>
-      </Navbar>
-      <React.StrictMode>
-        <Menu/>
-      </React.StrictMode>
-    </div>
-  );
+class App extends Component {
+  
+	constructor(props){
+		super(props);
+		this.state = {
+			dishes : DISHES 
+		};
+	}
+
+	render(){
+
+      return (
+        <div>
+      		<Navbar dark color="primary">
+		      <div className="container">
+      			<NavbarBrand href="/" >
+				    Ristorante Con Fusion
+	            </NavbarBrand>
+      		</div>
+      		</Navbar>
+      		<React.StrictMode>
+      			<Menu dishes={this.state.dishes}/>
+		    </React.StrictMode>
+      </div>
+      );
+    }
 }
 
 export default App;
